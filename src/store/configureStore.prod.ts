8ddcb18623fd77f006da3ab1946c96c21e05494a
Masaya Takahashi <masaya.oor710@gmail.com>
2018-08-20 01:34:52 +0900

    Introduce redux container

---
+++ src/store/configureStore.prod.ts
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from '../reducers/rootReducer';
+
+export const configureStore = preloadState => {
+  return createStore(rootReducer, preloadState, applyMiddleware(thunk));
+};
